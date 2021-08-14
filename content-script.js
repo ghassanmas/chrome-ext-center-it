a64413cca30c6605e0209341f2e90d4ead7d03f9
ghassanmas <ghassan.maslamani@gmail.com>
2021-08-14 18:15:16 +0300

    add basic files

---
+++ content-script.js
@@ -0,0 +1,26 @@
+
+var  status1 =  status1 || false; 
+var prevMargin;
+var prevWidth
+if (! status1){
+    //first time
+prevMargin = document.body.style.margin;
+prevWidth = document.body.style.width;
+status1 = true;
+document.body.style.margin="auto";
+document.body.style.width="800px"
+console.log("here should hanfe",status)
+}else{
+    console.log(typeof status);
+    console.log("three",status)
+
+    document.body.style.margin=prevWidth;
+    document.body.style.width=prevMargin;
+    status1=false;
+}
+
+console.log("helllo");
+console.log("this is ,",this);
+
+
+
